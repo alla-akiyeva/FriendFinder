889b9fba78c8d3d72e334faa89cfb387901bd9be
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-07-25 20:50:00 -0400

    fixing deployment bug

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+   }
 
 app.listen(PORT, function() {
     console.log("Listening on Port", PORT);
